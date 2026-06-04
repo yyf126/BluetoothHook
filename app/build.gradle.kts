4d400c3f3f94c5ae78110ca9b9a260cdb42d72c2
yyf126 <48576350+yyf126@users.noreply.github.com>
2026-06-04 12:34:48 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.jingyu233.bluetoothhook"
         minSdk = 29
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
